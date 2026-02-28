0ed85ab900db4a335b1c82d8cb0a2eb1654252a3
Mcamacho457 <camachomichael343@gmail.com>
2026-02-28 15:09:47 -0800

    Enrollment done and checked shapes

--- MiniFaceID.py
+++ MiniFaceID.py
@@ -2,6 +2,7 @@ import cv2
 import numpy as np
 from PCA import PCA
 from dataset_matrix import create_dataset_matrix
+from detect_crop_face import detect_crop_face
 
 modelFile = "models/res10_300x300_ssd_iter_140000.caffemodel"
 configFile = "models/deploy.prototxt"
@@ -16,15 +17,19 @@ class MiniFaceID:
         self.mean_face = None
         self.eigen_vals = []
         self.eigen_vects = []
+        self.eigen_faces = []
         self.auth_user_template = None
 
     def train_model(self):
         dataset_matrix = create_dataset_matrix(self.model, self.data)
         self.red_cov_mat, self.X_centered, self.mean_face, self.eigen_vals, self.eigen_vects = PCA(dataset_matrix)
+        #    (16384, N or really k)  =   (16384, N) x (N, N or really k)
+        # (will later change the eigen_vects to be (N, k) dimensions to only keep k-top eigen vectors)
+        self.eigen_faces = self.X_centered.T @ self.eigen_vects
 
         # get all the images for the authorized user, 5-10, and compute its metrics
         auth_user = []
-        for i in range(5):
+        for i in range(4):
             auth_user.append(dataset_matrix[i])
         auth_user = np.vstack(auth_user)
 
@@ -32,16 +37,32 @@ class MiniFaceID:
         auth_user_centered = auth_user - self.mean_face
 
         # project the centered images onto the PCA space
-        # auth_user_centered (5-10, 16384), eigen_vects (4, 16384)
-        auth_user_pca = auth_user_centered @ self.eigen_vects.T
+        # auth_user_centered (5-10, 16384), eigen_faces (16384, N or really k)
+        auth_user_pca = auth_user_centered @ self.eigen_faces
 
         # get the mean for the authorized user in the PCA space
         self.auth_user_template = np.mean(auth_user_pca, axis=0)
-    def enroll_face(photos):
+    def enroll_face(self, photos):
         # run detect_crop_face on every photo
-        return None
+        # photos is a list of the photos taken of the user to be enrolled as the new authorized user, len(photos) = 5-10
+        # flatten each photo to a vector of length 16384 and store in photos_matrix
+        photos_matrix = []
+        for i in range(len(photos)):
+            photo = photos[i]
+            crop_photo = detect_crop_face(self.model, photo)
+            # flatten each photo so that it can now be stored as a row in our matrix
+            flat_crop = crop_photo.flatten().astype(np.float32)
+            photos_matrix.append(flat_crop)
+        
+        # photos_matrix.shape = (5-10, 16384)
+        photos_matrix = np.vstack(photos_matrix)
+        enroll_centered = photos_matrix - self.mean_face
+
+        # weights.shape = (5-10, N or k)
+        weights = enroll_centered @ self.eigen_faces
+        self.auth_user_template = np.mean(weights, axis=0)
 
     def verify_face():
-        # verifies if the person is the authorized user or not
+        # verifies if the person is the authorized user or not. Do this by comparing user's template to authorized user's template
         response = False
         return response
--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plt
 from dataset_matrix import create_dataset_matrix
 from PCA import cov_matrix
 from PCA import eigen_decomp
+import MiniFaceID as FID
 
 modelFile = "models/res10_300x300_ssd_iter_140000.caffemodel"
 configFile = "models/deploy.prototxt"
@@ -12,7 +13,9 @@ dnn = cv2.dnn.readNetFromCaffe(configFile, modelFile)
 
 image_paths = ['train/n000002/0009_01.jpg', 'train/n000003/0003_01.jpg', 'train/n000004/0006_01.jpg', 'train/n000005/0006_01.jpg']
 
+"""
 dataset_matrix = create_dataset_matrix(dnn, image_paths)
+print(f'Dataset Matrix shape: {dataset_matrix.shape}')
 red_cov_mat, X_centered, mean_face = cov_matrix(dataset_matrix)
 eigen_vals, eigen_vects = eigen_decomp(red_cov_mat)
 
@@ -62,3 +65,90 @@ plt.show()
 
 print(f"Weights shape: {weights.shape}")
 print(f"Weight 1 shape: {weights[0].shape}")
+
+"""
+
+# test MiniFaceID class
+# test that class is constructed correctly (check the class attributes)
+miniFace = FID.MiniFaceID(image_paths)
+
+miniFace.train_model()
+print(f"Reduced Matrix shape: {miniFace.red_cov_mat.shape}")
+print(f"X_centered shape: {miniFace.X_centered.shape}")
+print(f"Mean face: {miniFace.mean_face.shape}")
+print(f"Eigen values shape: {miniFace.eigen_vals.shape}")
+print(f"Eigen vectors shape: {miniFace.eigen_vects.shape}")
+print(f"Eigen faces shape: {miniFace.eigen_faces.shape}")
+print(f"Authorized user template shape: {miniFace.auth_user_template.shape}")
+
+# test that we can enroll someone new
+cap = cv2.VideoCapture(0)
+
+while True:
+    ret, frame = cap.read()
+
+    if not ret:
+        break
+
+    (h, w) = frame.shape[:2]
+
+
+    # Creates blob (required preprocessing for DNN)
+    blob = cv2.dnn.blobFromImage(
+        # resizes frame to size expected by model (300x300)
+        cv2.resize(frame, (300, 300)),
+        # scales the pixels by 1 (doesn't do anything really)
+        1.0,
+        # makes sure the frame is correctly resized before being passed to the model as input
+        (300, 300),
+        # subtracts the mean for each channel from every pixel in the frame (blue_mu, green_mu, red_mu)
+        (104.0, 177.0, 123.0)
+    )
+
+    # sets this new blob as the input for the DNN
+    # blob = (batch, channels, height, width)
+    dnn.setInput(blob)
+    
+    # detections is (batch size, number classes(1 since we're detecting faces), number of detections, values per detection)
+    # detections = (1, 1, N, 7)
+    detections = dnn.forward()
+    cropped_face = frame
+
+    # Loop over detections (N dimension from above)
+    for i in range(detections.shape[2]):
+        # last dimension of detections has 7 values (image_id, class_id, confidence, x1, y1, x2, y2)
+        # gets confidence score for a specific detection
+        confidence = detections[0, 0, i, 2]
+
+        if confidence > 0.6:  # confidence threshold
+            box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
+            (startX, startY, endX, endY) = box.astype("int")
+
+            cropped_face = frame[startY:endY, startX:endX]
+
+            cv2.rectangle(frame, (startX, startY),
+                          (endX, endY),
+                          (0, 255, 0),
+                          2)
+
+    cv2.imshow("DNN Face Detector", frame)
+    # cv2.imshow("Resized Frame", cv2.resize(frame, (300, 300)))
+    # cv2.imshow("Cropped Face", cropped_face)
+
+    # image = blob[0]              # remove batch dimension
+    # image = image.transpose(1, 2, 0)  # CHW → HWC
+    # image = image + (104,177,123)     # add mean back
+    # image = image.astype("uint8")
+
+    # cv2.imshow("Blob Image", image)
+
+    if cv2.waitKey(1) & 0xFF == ord("q"):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
+
+
+
+
+# test that a new authorized user can be enrolled
